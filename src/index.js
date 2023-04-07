6cb020ab60c2f355ed69826d1dbb45696cc8d506
Eshan Mahajan <eshan@Eshan-Mahajan-MacBook.local>
2023-04-08 00:13:13 +0530

    Added basic body-parser and sql dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require ('./config/serverconfig');
 
+
 const startAndSetupServer = async() =>{
+    // create express object
     const app = express ();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+
 
     app.listen(PORT, ()=>{
         console.log(`Listening at ${PORT}`);
